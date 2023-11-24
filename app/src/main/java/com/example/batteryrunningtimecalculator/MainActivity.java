d7c93a9e7ff49d2aec4a6ccd50e51b919c87f125
michael <michael@youngees.net>
2023-11-24 15:31:52 +1030

    Updated to allow input of Total Battery Capacity

--- app/src/main/java/com/example/batteryrunningtimecalculator/MainActivity.java
+++ app/src/main/java/com/example/batteryrunningtimecalculator/MainActivity.java
@@ -15,13 +15,11 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText chargePercentEditText;
-    EditText dischargeRateEditText;
+    EditText chargePercentEditText, dischargeRateEditText;
     Button calculateButton;
     TextView timeRemainingTextView;
 
-    EditText chargeAmpHoursEditText;
-    EditText dischargeAmpsEditText;
+    EditText chargeAmpHoursEditText, totalAmpHoursEditText, dischargeAmpsEditText;
     Button calculateDischargeButton;
     TextView dischargeTimeRemainingTextView;
 
@@ -40,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         // discharge_amps_edittext
         // calculate_discharge_button
         // discharge_time_remaining_textview
+        totalAmpHoursEditText = findViewById(R.id.total_amphours_edittext);
         chargeAmpHoursEditText = findViewById(R.id.charge_amphours_edittext);
         dischargeAmpsEditText = findViewById(R.id.discharge_amps_edittext);
         calculateDischargeButton = findViewById(R.id.calculate_discharge_button);
@@ -58,38 +57,73 @@ public class MainActivity extends AppCompatActivity {
                 // Get the current time
                 Calendar calendar = Calendar.getInstance();
                 Date currentTime = calendar.getTime();
+                Date currentDay = calendar.getTime();
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.setTime(currentDay);
                 // Add time remaining in hours to the current time
                 calendar.add(Calendar.HOUR_OF_DAY, (int) timeRemaining);
                 Date newTime = calendar.getTime();
+                Date newDay = calendar.getTime();
+                Calendar calendar2 = Calendar.getInstance();
+                calendar2.setTime(newDay);
                 // Format the time as desired (e.g., "hh:mm a")
                 SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a", Locale.getDefault());
                 String formattedNewTime = sdf.format(newTime);
+                SimpleDateFormat sdfd = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                String formattedNewDate = sdfd.format(newTime);
+                int differenceInDays = calendar2.get(Calendar.DAY_OF_YEAR) - calendar1.get(Calendar.DAY_OF_YEAR);
+                String strtheDay = "unknown";
+                if (differenceInDays == 0) {
+                    strtheDay = " today";
+                } else if (differenceInDays == 1) {
+                    strtheDay = " tomorrow";
+                } else if (differenceInDays == 2 ) {
+                    strtheDay = " next day";
+                }
 
                 // display time remaining and expected time
-                timeRemainingTextView.setText("Time remaining: " + String.format("%.2f", timeRemaining) + " hours, empty at: " + formattedNewTime + " tomorrow");
+                timeRemainingTextView.setText("Time remaining: " + String.format("%.2f", timeRemaining) + " hours, empty at: " + formattedNewTime + strtheDay);
             }
         });
 
         calculateDischargeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                double totalAmpHours = Double.parseDouble(totalAmpHoursEditText.getText().toString());
                 double chargeAmpHours = Double.parseDouble(chargeAmpHoursEditText.getText().toString());
                 double dischargeAmps = Double.parseDouble(dischargeAmpsEditText.getText().toString());
 
-                double MiniumEnergy = 0.05 * 200; // in Amp Hours
-                double MinutesRemaining = (chargeAmpHours - MiniumEnergy) / dischargeAmps; //in hours
+                double MinimumEnergy = 0.05 * totalAmpHours; // in Amp Hours
+                double MinutesRemaining = (chargeAmpHours - MinimumEnergy) / dischargeAmps; //in hours
                 // Get the current time
                 Calendar calendar = Calendar.getInstance();
                 Date currentTime = calendar.getTime();
+                Date currentDay = calendar.getTime();
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.setTime(currentDay);
                 // Add time remaining in hours to the current time
                 calendar.add(Calendar.HOUR_OF_DAY, (int) MinutesRemaining);
                 Date newTime = calendar.getTime();
+                Date newDay = calendar.getTime();
+                Calendar calendar2 = Calendar.getInstance();
+                calendar2.setTime(newDay);
                 // Format the time as desired (e.g., "hh:mm a")
                 SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a", Locale.getDefault());
+                SimpleDateFormat sdfd = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
                 String formattedNewTime = sdf.format(newTime);
+                String formattedNewDate = sdfd.format(newTime);
+                int differenceInDays = calendar2.get(Calendar.DAY_OF_YEAR) - calendar1.get(Calendar.DAY_OF_YEAR);
+                String strtheDay = "unknown";
+                if (differenceInDays == 0) {
+                    strtheDay = " today";
+                } else if (differenceInDays == 1) {
+                    strtheDay = " tomorrow";
+                } else if (differenceInDays == 2 ) {
+                    strtheDay = " next day";
+                    }
 
                 // display time remaining and expected time
-                dischargeTimeRemainingTextView.setText("Time remaining: " + String.format("%.2f",MinutesRemaining) + " hours, empty at " + formattedNewTime + " tomorrow");
+                dischargeTimeRemainingTextView.setText("Time remaining: " + String.format("%.2f",MinutesRemaining) + " hours, empty at " + formattedNewTime + strtheDay);
             }
         });
     }
